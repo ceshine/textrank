0ba49fc7b152766d8c1acac3c9366741564c4cd1
CeShine Lee <shuanck@gmail.com>
2018-12-11 19:58:25 +0800

    Support Multiple Paragraphs

--- summa/syntactic_unit.py
+++ summa/syntactic_unit.py
@@ -1,14 +1,18 @@
 class SyntacticUnit(object):
 
-    def __init__(self, text, token=None, tag=None):
+    def __init__(self, text, token=None, tag=None, index=-1, paragraph=-1):
         self.text = text
         self.token = token
         self.tag = tag[:2] if tag else None  # just first two letters of tag
-        self.index = -1
+        self.index = index
         self.score = -1
+        self.paragraph = paragraph
 
     def __str__(self):
-        return "Original unit: '" + self.text + "' *-*-*-* " + "Processed unit: '" + self.token + "'"
+        return (
+            "Original unit: '" + self.text + "' *-* " + "Processed unit: '" +
+            self.token + "'" + "%d %d" % (self.paragraph, self.index)
+        )
 
     def __repr__(self):
         return str(self)
